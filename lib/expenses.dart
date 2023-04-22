bf2117ca4ee57b2331425e9f2e3b7f9edbe033d1
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-22 11:01:08 +0330

    create random data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -8,8 +10,22 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Fluuter Course',
+        amount: 20.25,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Some Bitches',
+        amount: 55.65,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: []),);
+    return Scaffold(
+      body: Column(children: []),
+    );
   }
 }
