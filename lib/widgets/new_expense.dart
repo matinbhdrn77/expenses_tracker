294f92a3331432b0f95b53b176154582586cb082
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-22 12:43:57 +0330

    add controllers and text fields

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 
-class NewExpense extends StatelessWidget {
+class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _amountController.dispose();
+    _titleController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(children: const [
+      child: Column(children: [
         TextField(
+          controller: _titleController,
           maxLength: 50,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             label: Text('Title'),
           ),
+        ),
+        TextField(
+          controller: _amountController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            prefixText: '\$',
+            label: Text('Amount'),
+          ),
+        ),
+        Row(
+          children: [
+            TextButton(onPressed: (){}, child: const Text('Cansel')),
+            ElevatedButton(onPressed: () {}, child: const Text('Save Expense'))
+          ],
         )
       ]),
     );
