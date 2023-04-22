a2df5d745e40b1b0e9c0c489bebe5ba6d125ecba
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-22 19:46:24 +0330

    dismmising expenses by swapping to left

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -41,6 +41,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +61,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(children: [
         const Text('The Chart'),
-        Expanded(child: ExpenseList(expenses: _registeredExpenses)),
+        Expanded(child: ExpenseList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,)),
       ]),
     );
   }
