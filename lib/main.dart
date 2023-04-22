46e51008272ef18ccb91d5bb42c3a953111a50f4
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-22 11:24:50 +0330

    resolving importing issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
-
+import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(MaterialApp(home: Expenses(),));
