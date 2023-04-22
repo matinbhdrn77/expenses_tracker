fbd46d075c2f4cb090e85db307a67b34bff86b7b
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-22 11:07:46 +0330

    import expensesList widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
@@ -25,7 +26,10 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: []),
+      body: Column(children: [
+        const Text('The Chart'),
+        Expanded(child: ExpenseList(expenses: _registeredExpenses)),
+      ]),
     );
   }
 }
