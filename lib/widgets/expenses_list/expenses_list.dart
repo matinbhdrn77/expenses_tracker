52f2d96212aafc0da49e0d44c60b9ac7dc1eddbd
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-23 10:51:31 +0330

    add red color when deleting a card

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -4,7 +4,8 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpenseList extends StatelessWidget {
-  const ExpenseList({super.key, required this.expenses, required this.onRemoveExpense});
+  const ExpenseList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
   final void Function(Expense expense) onRemoveExpense;
@@ -15,6 +16,11 @@ class ExpenseList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) {
         return Dismissible(
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.6),
+            margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+          ),
           key: ValueKey(expenses[index]),
           onDismissed: (direction) => onRemoveExpense(expenses[index]),
           child: ExpenseItem(
