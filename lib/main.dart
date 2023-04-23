87327414d7adcb393f311539867b611af77337ce
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-23 11:01:51 +0330

    add darkmode theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,27 @@ import 'package:expense_tracker/widgets/expenses.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+      useMaterial3: true,
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        color: kDarkColorScheme.secondaryContainer,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        ),
+      ),
+    ),
     theme: ThemeData().copyWith(
       useMaterial3: true,
       colorScheme: kColorScheme,
@@ -24,13 +43,14 @@ void main() {
         ),
       ),
       textTheme: ThemeData().textTheme.copyWith(
-        titleLarge: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: kColorScheme.onSecondaryContainer,
-          fontSize: 16,
-        ),
-      ),
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 16,
+            ),
+          ),
     ),
+    themeMode: ThemeMode.system,
     home: const Expenses(),
   ));
 }
